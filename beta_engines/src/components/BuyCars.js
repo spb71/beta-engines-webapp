c4f03f2d28859c69799b20b814bf7df631c27cbe
Mohammed Sajjad <mohammedsajjad1017@gmail.com>
2022-09-13 10:35:41 -0500

    Set Up

---
+++ beta_engines/src/components/BuyCars.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const BuyCars = props => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+
+
+export default BuyCars;
---
+++ beta_engines/src/components/UpdateCars.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Update = props => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+
+
+export default Update;
