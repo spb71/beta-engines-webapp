a73fce437ef0ee90207b43214d2d4f4d737c65d0
Mohammed Sajjad <mohammedsajjad1017@gmail.com>
2022-09-14 11:32:13 -0500

    URI

--- beta_engines/src/components/Nav.js
+++ beta_engines/src/components/Nav.js
@@ -6,7 +6,7 @@ const Nav = props => {
     return (
         <div>
 
-            <nav className="navbar navbar-expand-lg bg-light">
+            <nav className="navbar navbar-dark navbar-expand-lg bg-dark">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">Beta Engines</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,9 +26,9 @@ const Nav = props => {
                             </li>
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown link
+                                    Profile
                                 </a>
-                                <ul className="dropdown-menu">
+                                <ul className="dropdown-menu dropdown-menu-dark">
                                     <Link className="nav-link" to="buy">Buy Cars</Link>
                                     <Link className="nav-link" to="sell">Sell Cars</Link>
                                     <Link className="nav-link" to="update">Update Sale Information</Link>
